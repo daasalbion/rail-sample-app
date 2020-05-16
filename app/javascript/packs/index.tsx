ec572503b0237dfded3ac1ff70a6ed70a5db6104
Derlis Arguello <derlisarguello01@gmail.com>
2020-05-16 16:46:54 -0400

    test component added

---
+++ app/javascript/packs/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Test = () => {
+    return (<div>hola</div>);
+}
+
+ReactDOM.render(
+    <Test />,
+    document.getElementById('root')
+);
